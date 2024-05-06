d915fed82439600b3745478f54f09b074af5c410
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-06 14:43:06 -0400

    Move user-related routes under users/

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const user_controller = require("../controllers/userController");
-
 /* GET home page. */
 router.get('/', (req, res, next) => {
     res.render('index', { title: 'Express' });
 });
-
-router.get('/sign-up', (req, res, next) => {
-    res.render('signup_form', { title: 'Sign Up' });
-});
-
-router.post('/sign-up', user_controller.user_create_post);
-
-router.get('/join-club', (req, res, next) => {
-    res.render('join_club', { title: 'Join the Club' });
-});
-
-router.post('/join-club', user_controller.user_join_club_post);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+const user_controller = require("../controllers/userController");
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/sign-up', (req, res, next) => {
+    res.render('signup_form', { title: 'Sign Up' });
+});
+
+router.post('/sign-up', user_controller.user_create_post);
+
+router.get('/join-club', (req, res, next) => {
+    res.render('join_club', { title: 'Join the Club' });
+});
+
+router.post('/join-club', user_controller.user_join_club_post);
 
 module.exports = router;
