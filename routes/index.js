d250ba1a8f2ac3e3fc7957dab9c79b54a9fa54c1
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-06 13:24:34 -0400

    Add route for signup POST method

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const user_controller = require("../controllers/userController");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+    res.render('index', { title: 'Express' });
 });
+
+router.get('/sign-up', (req, res, next) => {
+    res.render('signup_form', { title: 'Sign Up' });
+});
+
+router.post('/sign-up', user_controller.user_create_post);
 
 module.exports = router;
