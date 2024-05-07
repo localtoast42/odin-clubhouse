d0a5a5b07cc3ca5b7cbf9418cfd03cf920f50eb4
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-07 11:39:37 -0400

    Add login, logout endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const passport = require("passport");
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.render('index', { title: 'Express' });
+    console.log(req.user);
+    res.render('index', { title: 'Home', user: req.user });
+});
+
+router.get('/login', (req, res, next) => {
+    res.render('login', { title: 'Login' });
+});
+
+router.post('/login',
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/login"
+    })
+);
+
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect("/");
+    });
 });
 
 module.exports = router;
