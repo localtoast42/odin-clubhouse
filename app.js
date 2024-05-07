fdc8c1e21f6a9f0f5f2cff15a6cb84a964b8f284
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-07 11:40:02 -0400

    Implement passport LocalStrategy authentication

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ require('dotenv').config()
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const session = require("express-session");
+const MongoStore = require('connect-mongo');
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
+const LocalStrategy = require("passport-local").Strategy;
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const helmet = require("helmet");
+const User = require("./models/user");
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
-const helmet = require("helmet");
 
 const app = express();
 
@@ -23,11 +28,9 @@ app.use(
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 const mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB);
 
-main().catch((err) => console.log(err));
-async function main() {
-    await mongoose.connect(mongoDB);
-}
+const mongoClient = mongoose.connection.getClient();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -38,6 +41,47 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: process.env.APP_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ client: mongoClient }),
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24,
+    }
+}));
+app.use(passport.session());
+
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const user = await User.findOne({ username: username });
+            if (!user) {
+                return done(null, false, { message: "Incorrect username" });
+            };
+            const match = await bcrypt.compare(password, user.password);
+            if (!match) {
+                return done(null, false, { message: "Incorrect password" });
+            };
+            return done(null, user);
+        } catch(err) {
+            return done(err);
+        };
+    })
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch(err) {
+        done(err);
+    };
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
