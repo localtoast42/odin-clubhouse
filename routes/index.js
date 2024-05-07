751e048c788f0f54e98fc121eb700fe1d579ca6d
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-07 12:34:41 -0400

    Remove console.log statement

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const passport = require("passport");
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    console.log(req.user);
     res.render('index', { title: 'Home', user: req.user });
 });
 
