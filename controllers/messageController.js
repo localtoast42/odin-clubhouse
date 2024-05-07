4b28742b5f0ee62f6c4124a779a3715c321e7e0f
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-07 17:08:15 -0400

    Add messageController

---
+++ controllers/messageController.js
@@ -0,0 +1,45 @@
+const Message = require("../models/message");
+const { body, validationResult } = require("express-validator");
+const asyncHandler = require("express-async-handler");
+
+exports.message_create_post = [
+    body("title")
+        .trim()
+        .isLength( { min: 1 })
+        .escape()
+        .withMessage("Title must be provided."),
+    body("message_text")
+        .trim()
+        .isLength( { min: 1 })
+        .escape()
+        .withMessage("Message must be provided."),
+
+    asyncHandler(async (req, res, next) => {
+        console.log(req.body);
+        const errors = validationResult(req);
+
+        const message = new Message({
+            title: req.body.title,
+            author: req.user.id,
+            text: req.body.message_text,
+            timestamp: Date.now(),
+        });
+
+        if (!errors.isEmpty()) {
+            res.render("create_message_form", {
+                title: "Create New Message",
+                message: message,
+                user: req.user,
+                errors: errors.array(),
+            });
+            return;
+        } else {
+            await message.save();
+            res.redirect('/');
+        };
+    }),
+];
+
+exports.message_update_post = [];
+
+exports.message_delete_post = {};
